b30cddc31dd7e927bb10eff9a7e15f662c1e381d
Eric Lovell <ericlovell@Erics-MacBook-Pro.local>
2019-01-11 23:12:55 -0500

    Missed some removals from last commit

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Search } from './components/Search';
 
 export default class App extends Component {
@@ -13,8 +11,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/Search' component={Search} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
       </Layout>
     );
   }
