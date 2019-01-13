4a86fd9811c9536226add253d6b9867a55122632
Eric Lovell <ericlovell@Erics-MacBook-Pro.local>
2019-01-12 22:00:33 -0500

    Moved dependency creation to constructor

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -16,16 +16,23 @@ namespace tramlaw_test.Controllers
             public string Query { get; set;}
         }
 
+        private readonly Settings Settings;
+        private readonly IRestClient APIClient;
+
+        public SearchController() 
+        {
+            Settings = new Settings();
+            APIClient = new RestClient(Settings.WalmartBaseURL);
+        }
+
         [HttpGet("/api/Search")]
         public IActionResult Search([FromQuery]SearchParams param)
         {
-            var settings = new Settings();
-            var client = new RestClient(settings.WalmartBaseURL);
             var request = new RestRequest("search", Method.GET);
-            request.AddParameter("apiKey", settings.WalmartAPIKey);
+            request.AddParameter("apiKey", Settings.WalmartAPIKey);
             if(!string.IsNullOrWhiteSpace(param.Query))
                 request.AddParameter("query", param.Query);
-            var response = client.Execute(request);
+            var response = APIClient.Execute(request);
             if(response.StatusCode == (HttpStatusCode)200)
                 return Ok(response.Content);
             return BadRequest(response.Content);
