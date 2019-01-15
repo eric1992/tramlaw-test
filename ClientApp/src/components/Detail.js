7df6d2b23caa298ba3d16c842454aeb90a9b45b4
Eric Lovell <ericlovell@Erics-MacBook-Pro.local>
2019-01-15 17:49:36 -0500

    Removed console log call

--- ClientApp/src/components/Detail.js
+++ ClientApp/src/components/Detail.js
@@ -14,7 +14,6 @@ const override = css`
 export class Detail extends Component {
     constructor(props){
         super(props);
-        console.log(props);
         this.state = {
             detail: null,
             recommendations: [],
