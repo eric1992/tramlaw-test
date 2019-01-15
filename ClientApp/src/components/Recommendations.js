04809dc467ab9dcf56907ff635f501893cebfc53
Eric Lovell <ericlovell@Erics-MacBook-Pro.local>
2019-01-15 14:12:41 -0500

    Added Recommendations test

--- ClientApp/src/components/Recommendations.js
+++ ClientApp/src/components/Recommendations.js
@@ -10,10 +10,10 @@ export class Recommendations extends Component {
     render() {
         return (
             <Container className={'appFont'}>
-                {!this.props.recommendations || !this.props.recommendations.length
-                    ? <Row><span>There are no recommendations</span></Row>
-                    : <Row>{this.props.recommendations.map((recommendation, i) => <Recommendation key={i} recommendation={recommendation}/>)}</Row>
-                }
+                <Row>{!this.props.recommendations || !this.props.recommendations.length
+                    ? <span>There are no recommendations</span>
+                    : this.props.recommendations.map((recommendation, i) => <Recommendation key={i} recommendation={recommendation}/>)
+                }</Row>
             </Container>
         )
     }
---
+++ ClientApp/src/components/Recommendations.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Recommendations } from './Recommendations';
+import { Recommendation } from './Recommendation';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+it('renders without prop recommendations', () => {
+    const wrapper = shallow(<Recommendations />);
+    const error = <span>There are no recommendations</span>;
+    expect(wrapper.contains(error)).toEqual(true);
+});
+
+it('renders with empty prop recommendations', () => {
+    const wrapper = shallow(<Recommendations recommendations={[]}/>);
+    const error = <span>There are no recommendations</span>;
+    expect(wrapper.contains(error)).toEqual(true);
+});
